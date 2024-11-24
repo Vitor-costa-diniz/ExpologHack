c785ff6218fefeb6154be9c2205f976f2313dd46
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2024-11-24 10:21:23 -0300

    fix 2 more sections

--- ExpologHack/ExpologHack/Features/View/Freight/ShowResult.swift
+++ ExpologHack/ExpologHack/Features/View/Freight/ShowResult.swift
@@ -9,7 +9,178 @@ import SwiftUI
 
 struct ShowResult: View {
     var body: some View {
-        Text("Testando")
+        ZStack {
+            Color.neutral600
+                .ignoresSafeArea()
+            
+            ScrollView {
+                VStack {
+                    routeStopsInfo
+                        .padding(.top, -2)
+                    
+                    driverCode
+                    
+                    fuelInfos
+                }
+                .padding(.top, 24)
+                .navigationTitle("Detalhes da entrega")
+            }
+        }
+    }
+    
+    private var routeStopsInfo: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            HStack {
+                Image(.miniTruck)
+                    .frame(width: 52, height: 52)
+                    .background(
+                        Circle()
+                            .foregroundStyle(Color.primary200)
+                    )
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Duração da viagem: 1d 22hrs e 32min")
+                        .font(.custom(TokenFont.medium.rawValue, size: 14))
+                        .foregroundStyle(Color.neutral0)
+                    
+                    Text("Frete #38422")
+                        .font(.custom(TokenFont.medium.rawValue, size: 20))
+                        .foregroundStyle(Color.neutral0)
+                }
+                Spacer()
+            }
+            
+            VStack(alignment: .leading, spacing: 12) {
+                ForEach(FreightUpdate.mockData) { update in
+                    HStack(alignment: .top, spacing: 12) {
+                        update.iconName
+                            .frame(width: 52, height: 52)
+                            .background(
+                                Circle()
+                                    .foregroundStyle(Color.primary200)
+                            )
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(update.status)
+                                .font(.custom(TokenFont.medium.rawValue, size: 16))
+                                .foregroundStyle(Color.neutral0)
+                            
+                            Text(update.location)
+                                .font(.custom(TokenFont.regular.rawValue, size: 13))
+                                .foregroundStyle(Color.neutral0)
+                            
+                            Text("Atualização realizada em: \(update.updateTime)")
+                                .font(.custom(TokenFont.regular.rawValue, size: 10))
+                                .foregroundStyle(Color.neutral100)
+                        }
+                        Spacer()
+                    }
+                    .padding(.vertical, 8)
+                    .cornerRadius(8)
+                }
+            }
+        }
+        .padding()
+        .background(
+            Color.cardBackground
+                .clipShape(.rect(cornerRadius: 16))
+        )
+        .padding(.horizontal, 16)
+    }
+    
+    private var driverCode: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Código do cliente")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    Text("#500424")
+                        .font(.body)
+                        .bold()
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Mercadoria")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    Text("Frutas")
+                        .font(.body)
+                        .bold()
+                        .foregroundColor(.blue)
+                }
+                Spacer()
+            }
+            
+            HStack(alignment: .lastTextBaseline) {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Pacotes")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    Text("20 pallets")
+                        .font(.body)
+                        .bold()
+                }
+                Spacer()
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Peso")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    Text("15000 Quilos")
+                        .font(.body)
+                        .bold()
+                }
+            }
+        }
+        .padding()
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(12)
+    }
+    
+    private var fuelInfos: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Consumo médio de combustível")
+                    .font(.custom(TokenFont.medium.rawValue, size: 16))
+                    .foregroundStyle(.neutral0)
+                
+                Text("3,6km/L")
+                    .font(.custom(TokenFont.medium.rawValue, size: 16))
+                    .foregroundStyle(.neutral100)
+                    .padding(.bottom, 12)
+                
+                Text("Emissão total de CO₂")
+                    .font(.custom(TokenFont.medium.rawValue, size: 16))
+                    .foregroundStyle(.neutral0)
+                
+                Text("2,3 toneladas de CO₂")
+                    .font(.custom(TokenFont.medium.rawValue, size: 16))
+                    .foregroundStyle(.neutral100)
+                    .padding(.bottom, 12)
+                
+                Text("Custo médio da viagem: R$ 6624,00")
+                    .font(.custom(TokenFont.medium.rawValue, size: 16))
+                    .foregroundStyle(.neutral0)
+                
+                Text("Combustível: R$ 5.142,00")
+                    .font(.custom(TokenFont.medium.rawValue, size: 16))
+                    .foregroundStyle(.neutral100)
+                
+                Text("Pedágios: R$ 1.500,00.")
+                    .font(.custom(TokenFont.medium.rawValue, size: 16))
+                    .foregroundStyle(.neutral100)
+            }
+            
+            Spacer()
+        }
+        .padding()
+        .background(
+            Color.cardBackground
+                .clipShape(.rect(cornerRadius: 16))
+        )
+        .padding(.horizontal, 16)
     }
 }
 
